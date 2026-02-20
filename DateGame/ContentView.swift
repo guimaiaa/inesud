fa745f72aa1caf313acf20888bbcd855a03e55a8
guimaiaa <guilhermemaiagirao@gmail.com>
2026-02-19 22:37:37 -0300

    Initial Commit

---
+++ DateGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DateGame
+//
+//  Created by Guilherme Maia on 19/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DateGame/DateGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  DateGameApp.swift
+//  DateGame
+//
+//  Created by Guilherme Maia on 19/02/26.
+//
+
+import SwiftUI
+
+@main
+struct DateGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
